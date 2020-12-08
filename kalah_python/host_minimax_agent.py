8b8fef09a9acf46dde7e106bf5a125d15ec76ff5
Stefan Dragos <ciocatedragos@gmail.com>
2020-12-08 13:37:47 +0100

    Updated the boilerplate code to a stub implementation.

--- kalah_python/host_minimax_agent.py
+++ kalah_python/host_minimax_agent.py
@@ -6,8 +6,27 @@ from config import HOST, PORT
 
 
 class MiniMaxAgent(Agent):
-    def minimax_first_attempt(self, moves: List[Action]) -> Action:
-        raise NotImplemented
+
+    # pseudocode to be discussed
+    def chooseMiniMaxMove(NodeClass, gnode, maxDepth=6):
+        "Choose bestMove for gnode along w final value"
+        if gnode.depth < maxDepth and not gnode.over():
+            for move in gnode.moves:
+                nxtGnode = NodeClass(gnode.board, gnode.player, gnode.depth + 1)
+                nxtGnode.move(move)
+                chooseMiniMaxMove(NodeClass, nxtGnode, maxDepth)  # recursion here
+                keep = (gnode.next == None)  # 1st of sequence
+                if gnode.maximizing():
+                    if keep or nxtGnode.value > gnode.value:
+                        gnode.value = nxtGnode.value
+                        gnode.next = nxtGnode
+                        gnode.bestMove = move
+                else:
+                    if keep or nxtGnode.value < gnode.value:
+                        gnode.value = nxtGnode.value
+                        gnode.next = nxtGnode
+                        gnode.bestMove = move
+        return gnode
 
     @overrides
     def decide_on_action(self, possible_actions: Dict[str, Action]) -> Action:
